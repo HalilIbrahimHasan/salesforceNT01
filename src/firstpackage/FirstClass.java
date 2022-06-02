e6765f9548e756cc5f272d33c8826d819204d159
Emre Firat <emre1242@gmail.com>
2022-06-01 21:46:38 -0400

    emres update

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -8,6 +8,8 @@ public class FirstClass {
 
     //We will do git collaboration using GitHub
     //be ready for them!
+
+    //and this is emre s update :))
     //This is ibrahim updates
 
     //hello everyone this is Red1!
