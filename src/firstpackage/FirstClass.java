238d74a2922f3c63fe38653e3b4eaa5f47023938
Ramazan Ozbakir <ramazanozbakirabd@gmail.com>
2022-06-01 20:56:06 -0500

    delete

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,21 +4,8 @@ public class FirstClass {
 
 
 
-    //Hello dear Developers!
-
-    //We will do git collaboration using GitHub
-    //be ready for them!
-
-    // hello everyone this Ramazan
-
-
-    //and this is emre s update :))
-    //This is ibrahim updates
-
-    //hello everyone this is Red1!
-
 
     //hello this is ramazan
-    // nasil egleniyor muyuz?
+    // I deleted everything
 
 }
