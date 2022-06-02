3b7b2e25c208868704c582d27773eb0014101cd9
Ramazan Ozbakir <ramazanozbakirabd@gmail.com>
2022-06-01 20:51:34 -0500

    rami

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -8,16 +8,16 @@ public class FirstClass {
 
     //We will do git collaboration using GitHub
     //be ready for them!
-<<<<<<< HEAD
+
     // hello everyone this Ramazan
-=======
+
 
     //and this is emre s update :))
     //This is ibrahim updates
 
     //hello everyone this is Red1!
 
->>>>>>> 992ffcf27e28fe22a2cffeade9b12568b51b35b9
+
     //hello this is ramazan
     // nasil egleniyor muyuz?
 
