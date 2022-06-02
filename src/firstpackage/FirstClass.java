bbcc1b9b86d7052fefd191d0c5ff28acf4538be2
Nurgul Aras <nurgularas@gmail.com>
2022-06-01 22:10:20 -0400

    last

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -1,9 +1,9 @@
 package firstpackage;
 
 public class FirstClass {
-    public static void main(String[] args) {
-        System.out.print("Hello from Houston!");
-    }
+//    public static void main(String[] args) {
+//        System.out.print("Hello from Houston!");
+//    }
 
 
 
